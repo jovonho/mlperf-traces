02f1a8f141d2e4eeabaf0bd24529118ff2704b51
Loic Ho-Von <loic.hovon@gmail.com>
2022-01-31 15:19:17 -0500

    updated start_traces

--- start_traces.sh
+++ start_traces.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-mkdir output/
-bpftrace -o ./output/bio.out ./trace_bio.bt &
-echo $!
-bpftrace -o ./output/vfs_rw.out ./trace_vfs_rw.bt &
-echo $!
-bpftrace -o ./output/open.out ./trace_open.bt &
-echo $!
+if [ $# -eq 0 ]
+then
+    output_dir="./output"
+else
+    output_dir=$1
+fi
 
+if [ ! -d $output_dir ] 
+then
+    mkdir -p $output_dir
+fi
+
+sudo bpftrace -o "$output_dir/bio.out" ./trace_bio.bt &
+echo $!
+sudo bpftrace -o "$output_dir/vfs_rw.out" ./trace_vfs_rw.bt &
+echo $!
+sudo bpftrace -o "$output_dir/open.out" ./trace_open.bt &
+echo $!
