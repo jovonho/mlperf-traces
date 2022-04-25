ec65848bcc37795e09ba656c3a1c4025443c03f4
Loic Ho-Von <loic.hovon@gmail.com>
2022-04-25 17:44:56 -0400

    Split off traces in different files

--- train_2min.sh
+++ train_2min.sh
@@ -23,11 +23,22 @@ tmux new-session -d -s training
 
 
 # Start the bpf traces, storing their pid
-bpftrace trace_vfs.bt -o ${output_dir}/trace_vfs.out &
-trace_vfs_pid=$!
-
 bpftrace trace_bio.bt -o ${output_dir}/trace_bio.out &
 trace_bio_pid=$!
+
+bpftrace trace_read_write.bt -o ${output_dir}/trace_read_write.out &
+trace_read_write_pid=$!
+
+bpftrace trace_create_del.bt -o ${output_dir}/trace_create_del.out &
+trace_create_del_pid=$!
+
+bpftrace trace_open_close.bt -o ${output_dir}/trace_open_close.out &
+trace_open_close_pid=$!
+
+bpftrace trace_mmap.bt -o ${output_dir}/trace_mmap.out &
+trace_mmap_pid=$!
+
+# Start the CPU and GPU monitoring
 
 mpstat 1 > ${output_dir}/cpu.out &
 trace_cpu_pid=$!
@@ -39,7 +50,6 @@ trace_gpu_pid=$!
 # Start training within the tmux session. 
 tmux send-keys -t training "/mlcommons_training/image_segmentation/pytorch/start_training.sh $2" C-m
 
-
 sleep 1
 
 # Get the system-wide PID of the root process ID in the container (bash)
@@ -50,19 +60,24 @@ while [ -z "$root_pid" ]
 do
 	echo "failed to get training pid, trying again"
 	sleep 1
-	root_pid=$(grep -E "NSpid:[[:space:]]+[0-9]+[[:space:]]+1[0-9]$" /proc/*/status 2> /dev/null | awk '{print $2}')
+	root_pid=$(grep -E "NSpid:[[:space:]]+[0-9]+[[:space:]]+1$" /proc/*/status 2> /dev/null | awk '{print $2}')
 	echo "new try: $root_pid"
 done
 
-# Attach strace to it (will automatically attach to all child processes)
+# Attach the syscall trace to the root_process 
+# It will automatically attach to all spawned child processes
 strace -ttt -f -p $root_pid -e 'trace=!ioctl,clock_gettime,sched_yield,nanosleep,sched_getaffinity,sched_setaffinity,futex,set_robust_list' -o ${output_dir}/strace.out &
 
 sleep 120
 
 # Kill the training process and the traces
+# Strace was stopped when root_pid ended
 ./kill_training.sh
-kill $trace_vfs_pid
 kill $trace_bio_pid
+kill $trace_read_write_pid
+kill $trace_create_del_pid
+kill $trace_open_close_pid
+kill $trace_mmap_pid
 kill $trace_cpu_pid
 kill $trace_gpu_pid
 
