9e28090727309912c8c3dec81f0f6a99c4edcea4
lhovon <loic.hovon@gmail.com>
2022-04-25 14:44:37 +0000

    AWS VM setup

--- stop_training.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo docker kill training
-sudo docker rm training
-
-tmux kill-session -t training
--- train_2min.sh
+++ train_2min.sh
@@ -1,41 +1,42 @@
 #!/bin/bash
 
-# Clean-up from a previous session
-#sudo docker kill training
-#sudo docker rm training
+# Force running as root
+if [ "${EUID:-$(id -u)}" -ne 0 ]
+then
+	sudo su -
+fi
+
+# Clean-up from a previous session if needed
 tmux kill-session -t training
 
 # Start a new tmux session 
 tmux new-session -d -s training 
 
-sleep 1
+# Start the bpf traces, storing their pid
+bpftrace trace_vfs.bt -o ${output_dir}/trace_vfs.out &
+trace_vfs_pid=$!
+
+bpftrace trace_bio.bt -o ${output_dir}/trace_bio.out &
+trace_bio_pid=$!
 
 # Start training within the tmux session.
 # Docker runs in detached mode, so in the background 
 tmux send-keys -t training "sudo docker exec -it -t training /bin/bash run_and_time.sh 1" C-m
 
 sleep 1
-#tmux send-keys -t training "bash run_and_time.sh 1" C-m
 
-#sleep 2
-
-#pid=$(sudo docker exec training cat /workspace/unet3d/proc.pid)
-#echo $pid
-
-#sleep 1
-#sudo docker exec training ps aux
-
-
+# Get the training process root pid
 root_pid=`grep -E "NSpid:[[:space:]]+[0-9]+[[:space:]]+1[0-9]$" /proc/*/status 2> /dev/null | awk '{print $2}'`
 echo $root_pid
 
+# Attach strace to it (will automatically attach to all child processes)
 strace -ttt -f -p $root_pid -e 'trace=!sched_yield,nanosleep,sched_getaffinity,sched_setaffinity,futex,set_robust_list' -o /data/MLIO/results/strace4/strace_1worker.out &
-
-bpftrace cd.bt -o /data/MLIO/results/strace4/cd_trace_1worker.out &
-trace_pid=$!
 
 sleep 120
 
-./kill_training.sh && kill $trace_pid
+# Kill the training process and the traces
+./kill_training.sh
+kill $trace_vfs_pid
+kill $trace_vio_pid
 
 exit 0
